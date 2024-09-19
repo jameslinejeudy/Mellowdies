457976d61100dcd790c4798b164dc66ff164ae3c
jameslinejeudy <jjeudy@ufl.edu>
2024-09-19 10:46:39 -0400

    hi

--- mellowdies/src/App.js
+++ mellowdies/src/App.js
@@ -1,12 +1,20 @@
 // App.js
 
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Homepage.js';
+import Landingpage from './Landingpage.js';
+
 
 function App() {
   return (
-        <Homepage/>
-  );
+    <Router>
+      <Routes>
+      <Route path="/" element={<Homepage />} />
+        <Route path="/landing" element={<Landingpage />} />
+      </Routes>
+    </Router> 
+ );
 }
 
 export default App;
